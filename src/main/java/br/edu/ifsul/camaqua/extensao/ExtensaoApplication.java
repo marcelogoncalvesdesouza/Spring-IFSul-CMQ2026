39063af7494a183bb8a2e9b8c6f2e0e39a675f54
Marcelo Gonçalves de Souza <marcelo.souza@dnweb.com.br>
2026-05-21 21:15:05 -0300

    Update.

---
+++ src/main/java/br/edu/ifsul/camaqua/extensao/ExtensaoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.camaqua.extensao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtensaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtensaoApplication.class, args);
+	}
+
+}
